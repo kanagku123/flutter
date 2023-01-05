fbf033aa7d609da7c89323fc48039705992fb0c9
kyungkyuNA <kanagku123@naver.com>
2023-01-05 16:22:11 +0900

    flutter class

--- lib/practice/pra5.dart
+++ lib/practice/pra5.dart
@@ -1,6 +1,20 @@
+class Player{
+  final String name;
+  int xp;
 
+  Player(this.name,this.xp);
 
+  void hello(){
+    print("my name is $name , my xp : $xp.`");
+}
+
+}
 
 void main() {
+  var player1 = Player("skrudrb", 1500);
+  var player2 = Player("나경규", 12000);
+  player1.hello();
+  player2.hello();
+
 
 }
